13be9433ffcb4935d7467ec34b3f7ff3594f85f1
Bogdan Bakarec <bbakarec16@raf.rs>
2019-10-03 22:20:46 +0200

    Refactoring

--- marc_parser.hs
+++ marc_parser.hs
@@ -3,6 +3,8 @@ import System.Environment
 import Data.Char
 import System.IO
 
+
+-- Grammar
 data Doc = Doc [Marc]
 
 data Marc = Marc Leader [ControlField] [DataField]
@@ -15,6 +17,8 @@ data DataField = DataField NumberTag Indicator Indicator [SubField]
 
 data SubField = SubField FieldTag FieldValue
 
+
+-- Type synonyms
 type LeaderTag = String
 type LeaderValue = String
 type NumberTag = String
@@ -24,6 +28,8 @@ type FieldTag = Char
 type FieldValue = String
 type NumTabs = Int
 
+
+-- Deriving the Show typeclass for writing in JSON format
 instance Show Doc where
     show (Doc records) = "[\n" ++ showRecords records ++ "]"
         where showRecords [] = ""
@@ -58,16 +64,21 @@ instance Show DataField where
 instance Show SubField where
     show (SubField tag value) = "\t\t\t\t\t\t\t" ++ show (charToString tag) ++ ":" ++ show value
 
+    
 charToString :: Char -> String
 charToString c = [c]
     
+    
+-- Parser
 document :: Parsec String Int Doc
 document = do spaces
               res <- many (try marc21 <|> try marc21congress <|> try marc21leader <|> try unimarc <|> try unimarcsame)
               spaces
               eof
               return (Doc res)
+
               
+-- Marc21              
 marc21 :: Parsec String Int Marc
 marc21 = do spaces
             ldr <- leader21
@@ -78,6 +89,39 @@ marc21 = do spaces
             spaces
             return (Marc ldr cntrl dataFlds)
             
+leader21 :: Parsec String Int Leader
+leader21 = do tag <- string "LDR"
+              spaces
+              value <- many (noneOf "\n")
+              return (Leader tag value)
+              
+cntFld21 :: Parsec String Int ControlField
+cntFld21 = do tag <- count 3 digit
+              space
+              space
+              value <- many (noneOf "\n")
+              spaces
+              return (ControlField tag value)
+              
+dataFld21 :: Parsec String Int DataField
+dataFld21 = do tag <- count 3 digit
+               spaces
+               ind1 <- char '#' <|> digit
+               ind2 <- char '#' <|> digit
+               spaces
+               subFlds <- many1 subFld21
+               return (DataField tag ind1 ind2 subFlds)
+
+subFld21 :: Parsec String Int SubField
+subFld21 = do char '$'
+              tag <- alphaNum
+              spaces
+              value <- many (noneOf "$\n")
+              spaces
+              return (SubField tag value)
+             
+             
+-- Marc21 - Library of Congress            
 marc21congress :: Parsec String Int Marc
 marc21congress = do spaces
                     ldr <- leader21congress
@@ -87,69 +131,13 @@ marc21congress = do spaces
                     dataFlds <- many (try dataFld21congress)
                     spaces
                     return (Marc ldr cntrl dataFlds)
-                    
-marc21leader :: Parsec String Int Marc
-marc21leader = do spaces
-                  ldr <- leader21leader
-                  spaces
-                  cntrl <- many (try cntFld21leader)
-                  spaces
-                  dataFlds <- many (try dataFld21leader)
-                  spaces
-                  return (Marc ldr cntrl dataFlds)
-                  
-unimarc :: Parsec String Int Marc
-unimarc = do spaces
-             let ldr = (Leader "" "")
-             spaces
-             unildr <- unimarcleader
-             spaces
-             dataFlds <- many1 (try dataFldunimarc)
-             spaces
-             return (Marc ldr [] (unildr:dataFlds))
-             
-unimarcsame :: Parsec String Int Marc
-unimarcsame = do spaces
-                 let ldr = (Leader "" "")
-                 spaces
-                 dataFlds <- many1 (try dataFldunimarcsame)
-                 return (Marc ldr [] dataFlds)
-              
-leader21 :: Parsec String Int Leader
-leader21 = do tag <- string "LDR"
-              spaces
-              value <- many (noneOf "\n")
-              return (Leader tag value)
-              
+
 leader21congress :: Parsec String Int Leader
 leader21congress = do tag <- string "000"
                       spaces
                       value <- many (noneOf "\n")
-                      return (Leader tag value)
-                      
-leader21leader :: Parsec String Int Leader
-leader21leader = do tag <- string "LEADER"
-                    spaces
-                    value <- many (noneOf "\n")
-                    return (Leader tag value)
-                    
-unimarcleader :: Parsec String Int DataField
-unimarcleader = do tag <- string "001"
-                   spaces
-                   ind1 <- char '#' <|> digit
-                   ind2 <- char '#' <|> digit
-                   spaces
-                   subFlds <- many subFldunimarc
-                   return (DataField tag ind1 ind2 subFlds)
-            
-cntFld21 :: Parsec String Int ControlField
-cntFld21 = do tag <- count 3 digit
-              space
-              space
-              value <- many (noneOf "\n")
-              spaces
-              return (ControlField tag value)
-              
+                      return (Leader tag value)                    
+
 cntFld21congress :: Parsec String Int ControlField
 cntFld21congress = do tag <- count 3 digit
                       spaces
@@ -162,26 +150,7 @@ cntFld21congress = do tag <- count 3 digit
                       value <- many (noneOf "\n")
                       spaces
                       return (ControlField tag value)
-                      
-cntFld21leader :: Parsec String Int ControlField
-cntFld21leader = do tag <- count 3 digit
-                    spaces
-                    lookAhead (do count 2 (noneOf "$")
-                                  spaces
-                                  noneOf "$")
-                    value <- many (noneOf "\n")
-                    spaces
-                    return (ControlField tag value)                  
-            
-dataFld21 :: Parsec String Int DataField
-dataFld21 = do tag <- count 3 digit
-               spaces
-               ind1 <- char '#' <|> digit
-               ind2 <- char '#' <|> digit
-               spaces
-               subFlds <- many1 subFld21
-               return (DataField tag ind1 ind2 subFlds)
-               
+
 dataFld21congress :: Parsec String Int DataField
 dataFld21congress = do tag <- count 3 digit
                        spaces
@@ -190,7 +159,43 @@ dataFld21congress = do tag <- count 3 digit
                        spaces
                        subFlds <- many1 subFld21congress
                        return (DataField tag ind1 ind2 subFlds)
-                       
+
+subFld21congress :: Parsec String Int SubField
+subFld21congress = do char '|'
+                      tag <- alphaNum
+                      spaces
+                      value <- sepBy (many (noneOf " |\n")) (string " ")
+                      spaces
+                      return (SubField tag (listToString value))
+            
+            
+-- Marc21 with LEADER                    
+marc21leader :: Parsec String Int Marc
+marc21leader = do spaces
+                  ldr <- leader21leader
+                  spaces
+                  cntrl <- many (try cntFld21leader)
+                  spaces
+                  dataFlds <- many (try dataFld21leader)
+                  spaces
+                  return (Marc ldr cntrl dataFlds)
+
+leader21leader :: Parsec String Int Leader
+leader21leader = do tag <- string "LEADER"
+                    spaces
+                    value <- many (noneOf "\n")
+                    return (Leader tag value)
+
+cntFld21leader :: Parsec String Int ControlField
+cntFld21leader = do tag <- count 3 digit
+                    spaces
+                    lookAhead (do count 2 (noneOf "$")
+                                  spaces
+                                  noneOf "$")
+                    value <- many (noneOf "\n")
+                    spaces
+                    return (ControlField tag value)                    
+
 dataFld21leader :: Parsec String Int DataField
 dataFld21leader = do tag <- count 3 digit
                      spaces
@@ -200,7 +205,36 @@ dataFld21leader = do tag <- count 3 digit
                      spaces
                      subFlds <- many1 subFld21leader
                      return (DataField tag ind1 ind2 subFlds)
-                     
+
+subFld21leader :: Parsec String Int SubField
+subFld21leader = do char '$'
+                    tag <- alphaNum
+                    spaces
+                    value <- sepBy (many (noneOf " $\n")) (string " ")
+                    spaces
+                    return (SubField tag (listToString value))
+      
+      
+-- UNIMARC                    
+unimarc :: Parsec String Int Marc
+unimarc = do spaces
+             let ldr = (Leader "" "")
+             spaces
+             unildr <- unimarcleader
+             spaces
+             dataFlds <- many1 (try dataFldunimarc)
+             spaces
+             return (Marc ldr [] (unildr:dataFlds))
+ 
+unimarcleader :: Parsec String Int DataField
+unimarcleader = do tag <- string "001"
+                   spaces
+                   ind1 <- char '#' <|> digit
+                   ind2 <- char '#' <|> digit
+                   spaces
+                   subFlds <- many subFldunimarc
+                   return (DataField tag ind1 ind2 subFlds) 
+
 dataFldunimarc :: Parsec String Int DataField
 dataFldunimarc = do lookAhead (do spaces
                                   notFollowedBy (string "001"))
@@ -211,40 +245,7 @@ dataFldunimarc = do lookAhead (do spaces
                     spaces
                     subFlds <- many1 subFldunimarc
                     return (DataField tag ind1 ind2 subFlds)
-                    
-dataFldunimarcsame :: Parsec String Int DataField
-dataFldunimarcsame = do tag <- count 3 digit
-                        ind1 <- digit <|> space
-                        ind2 <- digit <|> space
-                        subFlds <- many subFldunimarcsame
-                        many (char (toEnum 30))
-                        --spaces
-                        return (DataField tag ind1 ind2 subFlds)
-             
-subFld21 :: Parsec String Int SubField
-subFld21 = do char '$'
-              tag <- alphaNum
-              spaces
-              value <- many (noneOf "$\n")
-              spaces
-              return (SubField tag value)
-              
-subFld21congress :: Parsec String Int SubField
-subFld21congress = do char '|'
-                      tag <- alphaNum
-                      spaces
-                      value <- sepBy (many (noneOf " |\n")) (string " ")
-                      spaces
-                      return (SubField tag (listToString value))
-                      
-subFld21leader :: Parsec String Int SubField
-subFld21leader = do char '$'
-                    tag <- alphaNum
-                    spaces
-                    value <- sepBy (many (noneOf " $\n")) (string " ")
-                    spaces
-                    return (SubField tag (listToString value))
-                    
+
 subFldunimarc :: Parsec String Int SubField
 subFldunimarc = do char '['
                    tag <- alphaNum
@@ -252,14 +253,31 @@ subFldunimarc = do char '['
                    value <- many (noneOf "[\n")
                    spaces
                    return (SubField tag value)
+        
+        
+-- UNIMARC inline             
+unimarcsame :: Parsec String Int Marc
+unimarcsame = do spaces
+                 let ldr = (Leader "" "")
+                 spaces
+                 dataFlds <- many1 (try dataFldunimarcsame)
+                 return (Marc ldr [] dataFlds)                                                                                                      
+                    
+dataFldunimarcsame :: Parsec String Int DataField
+dataFldunimarcsame = do tag <- count 3 digit
+                        ind1 <- digit <|> space
+                        ind2 <- digit <|> space
+                        subFlds <- many subFldunimarcsame
+                        many (char (toEnum 30))
+                        return (DataField tag ind1 ind2 subFlds)                                        
                    
 subFldunimarcsame :: Parsec String Int SubField
 subFldunimarcsame = do char (toEnum 31)
                        tag <- alphaNum
                        value <- many (noneOf ([toEnum 31]++[toEnum 30]++"\n"))
-                       --space
                        return (SubField tag value)
 
+                       
 listToString :: [String] -> String
 listToString [] = ""
 listToString (x:xs)
@@ -267,19 +285,12 @@ listToString (x:xs)
     | head xs == "" = x
     | otherwise = x ++ " " ++ listToString xs
     
-unimarc_same31 :: Char -> Bool
-unimarc_same31 c
-    | ord c == 31 = False
-    | ord c == 30 = False
-    | ord c == 26 = False
-    | otherwise = True
             
 main :: IO ()
 main = do (input:output:[]) <- getArgs
           h <- openFile input ReadMode
           hSetEncoding h utf8
           cont <- hGetContents h
-          --cont <- readFile input
           case (runParser document 0 input cont) of
             Left err -> putStrLn . show $ err
             Right rss -> writeFile output . show $ rss
